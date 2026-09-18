eabb786949335aba33635c91a6d644acd941903b
Ignacio Soler <ignaciojsoler@gmail.com>
2026-09-18 16:26:35 -0300

    feat: update project description

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Local AI Lab",
-  WORDMARK: "HF/LOCAL-LAB",
+  NAME: "LOCAL AI LAB",
+  WORDMARK: "LOCAL-AI-LAB",
   SOURCE: "https://github.com/ignaciojsoler/local-ai-lab",
   EMAIL: "ignaciojsoler@gmail.com",
 };
